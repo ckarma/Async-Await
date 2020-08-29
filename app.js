1ef2c68dc2ed644ab3f19857ba5ccb3a71a198fa
karma chand <62472062+karma53@users.noreply.github.com>
2020-08-29 14:39:06 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
 
-// Write your code below:
 async function makeBeans() {
   let type = await shopForBeans();
   let isSoft = await soakTheBeans(type);
